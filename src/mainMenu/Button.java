2034fed4f8ac5cdfd40d4b510c00f4bc4840c7d8
pandesaal <asophiadd@gmail.com>
2024-07-10 04:47:07 +0800

    better looking MainMenuView

--- src/mainMenu/Button.java
+++ src/mainMenu/Button.java
@@ -1,16 +1,19 @@
 package mainMenu;
 
-import java.awt.Dimension;
+import java.awt.*;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Button extends JButton{
     
-    public Button(){
-        ImageIcon image = new ImageIcon(this.getClass().getResource("/assets/MainMenu/BUTTON.png"));
+    public Button(String text){
+        setText(text);
+        setFont(new Font("Sans Serif", Font.BOLD, 17));
+        setHorizontalTextPosition(JButton.CENTER);
+        ImageIcon image = new ImageIcon(new ImageIcon(this.getClass().getResource("/assets/MainMenu/BUTTON.png")).getImage().getScaledInstance(442/2, 121/2, Image.SCALE_DEFAULT));
         setIcon(image);
-        setPreferredSize(new Dimension(442, 121));
+        setSize(new Dimension(442/2, 121/2));
         setOpaque(false);
         setContentAreaFilled(false);
         setBorderPainted(false);
--- src/mainMenu/MainMenuView.java
+++ src/mainMenu/MainMenuView.java
@@ -3,11 +3,7 @@ package mainMenu;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Rectangle;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.io.File;
@@ -32,13 +28,17 @@ public class MainMenuView extends JFrame{
 
     private ImageIcon logoImage;
     private JLabel logoLabel;
-    
+    private JPanel hello;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
     public MainMenuView(){
 
         // background image
         try {
             bgImage = ImageIO.read(this.getClass().getResource("/assets/WINDOW_BACKGROUND.png"));
-            contentHolder = new JPanel(new BorderLayout()) {
+            contentHolder = new JPanel(new GridLayout(0, 2)) {
                 @Override public void paintComponent(Graphics g) {
                     g.drawImage(bgImage, 0, 0, this.getWidth(), this.getHeight(), this);
                 }
@@ -47,38 +47,34 @@ public class MainMenuView extends JFrame{
             throw new RuntimeException(e);
         }
 
-        leftSide = new JPanel();
-        leftSide.setOpaque(false);
-        rightSide = new JPanel(new BorderLayout());
+        rightSide = new JPanel(new GridLayout(4, 0));
         rightSide.setOpaque(false);
 
         // hotel image
-        hotelImage = new ImageIcon(this.getClass().getResource("/assets/MainMenu/HOTEL_GRAPHIC.png"));
-        hotelLabel = new JLabel("", hotelImage, JLabel.LEFT);
+        hotelImage = new ImageIcon(new ImageIcon(this.getClass().getResource("/assets/MainMenu/HOTEL_GRAPHIC.png")).getImage().getScaledInstance((int) (452), (int) (971), Image.SCALE_DEFAULT));
+        hotelLabel = new JLabel(hotelImage);
+        hotelLabel.setVerticalAlignment(JLabel.BOTTOM);
         hotelLabel.setVisible(true);
         hotelLabel.setOpaque(false);
-        leftSide.add(hotelLabel, BorderLayout.CENTER);
 
         // app name image
         logoImage = new ImageIcon(this.getClass().getResource("/assets/MainMenu/HOTEL_NAME.png"));
-        logoLabel = new JLabel("", logoImage, JLabel.RIGHT);
+        logoLabel = new JLabel(logoImage);
         logoLabel.setVisible(true);
-        logoLabel.setOpaque(false);
         rightSide.add(logoLabel);
 
         // buttons
-        buttonImage = new ImageIcon(this.getClass().getResource("/assets/MainMenu/BUTTON.png"));
-        createHotelButton = new Button();
-        manageHotelButton = new Button();
-        createReservationButton = new Button();
-        rightSide.add(createHotelButton, BorderLayout.CENTER);
-        rightSide.add(manageHotelButton, BorderLayout.CENTER);
-        rightSide.add(createReservationButton, BorderLayout.CENTER);
-        
-        
+        createHotelButton = new Button("Create Hotel");
+        manageHotelButton = new Button("Manage Hotel");
+        createReservationButton = new Button("Make Reservation");
+        rightSide.add(createHotelButton);
+        rightSide.add(manageHotelButton);
+        rightSide.add(createReservationButton);
+
+
         setTitle("CCPROG3 MCO: Hotel Reservation System (S27 Group 4)");
-        contentHolder.add(leftSide, BorderLayout.WEST);
-        contentHolder.add(rightSide, BorderLayout.EAST);
+        contentHolder.add(hotelLabel);
+        contentHolder.add(rightSide);
         setContentPane(contentHolder);
         setSize(1280, 720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -91,7 +87,4 @@ public class MainMenuView extends JFrame{
     public void addCreateHotelButtonListener(ActionListener listener) {createHotelButton.addActionListener(listener);}
 
 
-    public static void main(String[] args) {
-        new MainMenuView();
-    }
 }
