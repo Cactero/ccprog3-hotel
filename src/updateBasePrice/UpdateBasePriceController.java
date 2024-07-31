35716ef18235ebc9282f845612bba8ae35ac1676
pandesaal <asophiadd@gmail.com>
2024-07-31 10:43:01 +0800

    allbugsfixed(real)(true).jpeg

--- src/updateBasePrice/UpdateBasePriceController.java
+++ src/updateBasePrice/UpdateBasePriceController.java
@@ -33,46 +33,51 @@ public class UpdateBasePriceController extends AbstractController implements Pop
 
         boolean hasConflict;
         String basePriceString;
-        float basePrice = 0;
+        float basePrice;
 
-        JPanel updateBasePricePanel = new JPanel(new GridLayout(2, 2, 10, 10));
-        updateBasePricePanel.add(new JLabel("Current base price: "));
-        updateBasePricePanel.add(new JLabel(String.format("$%.2f", hotel.getBasePrice())));
+        if (hotel.getClientCount() > 0){
+            JOptionPane.showMessageDialog(null, "Unable to change base price due to existing reservations", "Reservations exist", JOptionPane.PLAIN_MESSAGE);
+            frame.switchView(((UpdateBasePriceModel) model).manageHotel());
+        } else {
+            JPanel updateBasePricePanel = new JPanel(new GridLayout(2, 2, 10, 10));
+            updateBasePricePanel.add(new JLabel("Current base price: "));
+            updateBasePricePanel.add(new JLabel(String.format("$%.2f", hotel.getBasePrice())));
 
-        updateBasePricePanel.add(new JLabel("Update base price?"));
+            updateBasePricePanel.add(new JLabel("Update base price?"));
 
-        int option = JOptionPane.showOptionDialog(null, updateBasePricePanel, "Update base price", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-        if (option == JOptionPane.OK_OPTION) {
+            int option = JOptionPane.showOptionDialog(null, updateBasePricePanel, "Update base price", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+            if (option == JOptionPane.OK_OPTION) {
 
-            basePriceString = JOptionPane.showInputDialog("Enter new base price:");
+                basePriceString = JOptionPane.showInputDialog("Enter new base price:");
 
-            try {
-                basePrice = Float.parseFloat(basePriceString);
+                try {
+                    basePrice = Float.parseFloat(basePriceString);
 
-                if (basePriceString.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Enter a new base price.", "No base price", JOptionPane.PLAIN_MESSAGE);
-                    promptUser();
-                } else {
-                    hasConflict = (hotel.getBasePrice() == basePrice) || basePrice < 100;
-
-                    if (!hasConflict) {
-                        ((UpdateBasePriceModel) model).setBasePrice(basePrice);
-                        JOptionPane.showMessageDialog(null, "Successfully updated base price to $" + basePriceString + "!", "Hotel " + hotel.getName(), JOptionPane.PLAIN_MESSAGE);
-                        frame.switchView(((UpdateBasePriceModel) model).manageHotel());
-                    } else if(basePrice < 100) {
-                        JOptionPane.showMessageDialog(null, "Base price must be at least $100.", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+                    if (basePriceString.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Enter a new base price.", "No base price", JOptionPane.PLAIN_MESSAGE);
+                        promptUser();
                     } else {
-                        JOptionPane.showMessageDialog(null, "Base price is already $" + basePriceString + ".", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+                        hasConflict = (hotel.getBasePrice() == basePrice) || basePrice < 100;
+
+                        if (!hasConflict) {
+                            ((UpdateBasePriceModel) model).setBasePrice(basePrice);
+                            JOptionPane.showMessageDialog(null, "Successfully updated base price to $" + basePriceString + "!", "Hotel " + hotel.getName(), JOptionPane.PLAIN_MESSAGE);
+                            frame.switchView(((UpdateBasePriceModel) model).manageHotel());
+                        } else if (basePrice < 100) {
+                            JOptionPane.showMessageDialog(null, "Base price must be at least $100.", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Base price is already $" + basePriceString + ".", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+                        }
+                        promptUser();
                     }
-                    promptUser();
+                } catch (NumberFormatException _) {
+                    JOptionPane.showMessageDialog(null, "Please enter a number.", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+                    frame.switchView(((UpdateBasePriceModel) model).manageHotel());
                 }
-            } catch (NumberFormatException _) {
-                JOptionPane.showMessageDialog(null, "Please enter a number.", "Invalid base price", JOptionPane.PLAIN_MESSAGE);
+
+            } else {
                 frame.switchView(((UpdateBasePriceModel) model).manageHotel());
             }
-
-        } else {
-            frame.switchView(((UpdateBasePriceModel) model).manageHotel());
         }
     }
 }
